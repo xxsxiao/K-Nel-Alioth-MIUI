d60ed6cf05dcc20fabe7e06dd8e5ed8d61163d07
Mikulas Patocka <mpatocka@redhat.com>
2021-02-10 15:26:23 -0500

    dm: fix deadlock when swapping to encrypted device
    commit a666e5c05e7c4aaabb2c5d58117b0946803d03d2 upstream.
    
    The system would deadlock when swapping to a dm-crypt device. The reason
    is that for each incoming write bio, dm-crypt allocates memory that holds
    encrypted data. These excessive allocations exhaust all the memory and the
    result is either deadlock or OOM trigger.
    
    This patch limits the number of in-flight swap bios, so that the memory
    consumed by dm-crypt is limited. The limit is enforced if the target set
    the "limit_swap_bios" variable and if the bio has REQ_SWAP set.
    
    Non-swap bios are not affected becuase taking the semaphore would cause
    performance degradation.
    
    This is similar to request-based drivers - they will also block when the
    number of requests is over the limit.
    
    Signed-off-by: Mikulas Patocka <mpatocka@redhat.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/device-mapper.h
+++ include/linux/device-mapper.h
@@ -317,10 +317,14 @@ struct dm_target {
 	bool split_discard_bios:1;
 
 	/*
+
 	 * Set if inline crypto capabilities from this target's underlying
 	 * device(s) can be exposed via the device-mapper device.
 	 */
 	bool may_passthrough_inline_crypto:1;
+	 /* Set if we need to limit the number of in-flight bios when swapping.
+	 */
+	bool limit_swap_bios:1;
 };
 
 /* Each target can link one of these into the table */
