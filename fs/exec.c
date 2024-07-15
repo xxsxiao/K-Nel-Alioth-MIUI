3e2d61e19ec4120af263137386cbabd77780e4b5
CCCC-L <993641484@qq.com>
2024-07-15 14:03:05 +0000

    Multi-Gen LRU

--- fs/exec.c
+++ fs/exec.c
@@ -1056,7 +1056,9 @@ static int exec_mmap(struct mm_struct *mm)
 	activate_mm(active_mm, mm);
 	if (IS_ENABLED(CONFIG_ARCH_WANT_IRQS_OFF_ACTIVATE_MM))
 		local_irq_enable();
+	lru_gen_use_mm(mm);
 	tsk->mm->vmacache_seqnum = 0;
+	lru_gen_add_mm(mm);
 	vmacache_flush(tsk);
 	task_unlock(tsk);
 	if (old_mm) {
