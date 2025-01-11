8c5ba8613c1d9bb3e9c201c66c1916df1a376c0f
KNKJ55 <knkj55@proton.me>
2025-01-11 02:49:35 +0000

    fallback: remove Kali Nethunter

--- android/Packer/customize.sh
+++ android/Packer/customize.sh
@@ -27,12 +27,9 @@ ui_print '打包 Boot 镜像。。。'
 ui_print '刷入 Boot 镜像。。。'
 dd if=new-boot.img of=/dev/block/by-name/boot_a
 dd if=new-boot.img of=/dev/block/by-name/boot_b
-if [ -e $MODPATH/dtbo.img ];
-then 
-    ui_print '刷入 Dtbo 镜像。。。'
-    dd if=$MODPATH/dtbo.img of=/dev/block/by-name/dtbo_a
-    dd if=$MODPATH/dtbo.img of=/dev/block/by-name/dtbo_b
-fi
+ui_print '刷入 Dtbo 镜像。。。'
+dd if=$MODPATH/dtbo.img of=/dev/block/by-name/dtbo_a
+dd if=$MODPATH/dtbo.img of=/dev/block/by-name/dtbo_b
 ui_print '安装完成'
 
 ui_print '清理中。。。'
