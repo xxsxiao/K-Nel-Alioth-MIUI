2817917f2ee397ab06eefb2dbf826f4958400ebe
KNKJ55 <knkj55@proton.me>
2025-01-11 13:18:18 +0000

    fs/devpts: extern int ksu_handle_devpts when ksu was enabled

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -599,7 +599,9 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 	return dentry;
 }
 
+#ifdef CONFIG_KSU
 extern int ksu_handle_devpts(struct inode*);
+#endif
 
 /**
  * devpts_get_priv -- get private data for a slave
@@ -609,7 +611,9 @@ extern int ksu_handle_devpts(struct inode*);
  */
 void *devpts_get_priv(struct dentry *dentry)
 {
+    #ifdef CONFIG_KSU
     ksu_handle_devpts(dentry->d_inode);
+    #endif
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
 	return dentry->d_fsdata;
